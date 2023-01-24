c516333800da21a3044e4df575e52336ad53cd44
Hackipw <91828495+Hackipw@users.noreply.github.com>
2023-01-24 20:03:27 +0530

    Update sejas.py

--- sejas.py
+++ sejas.py
@@ -10,7 +10,7 @@ import datetime
 from base64 import b64decode,b64encode
 from datetime import date
 
-expirydate = datetime.date(2023, 2, 23)
+expirydate = datetime.date(2023, 2, 24)
 #expirydate = datetime.date(2021, 8, 30)
 today=date.today()
 def hero():
